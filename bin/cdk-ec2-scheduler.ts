f5edc4cfcf877a1f0c028458e1ea1c7c45778f75
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2024-08-08 00:32:14 +0700

    feat: update cron tab

--- bin/cdk-ec2-scheduler.ts
+++ bin/cdk-ec2-scheduler.ts
@@ -17,7 +17,7 @@ new CdkEc2SchedulerStack(app, 'CdkEc2SchedulerStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '123456789012', region: 'us-east-1' },
-
+  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "558012790533", region: "ap-southeast-1" },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/cdk-ec2-scheduler-stack.ts
+++ lib/cdk-ec2-scheduler-stack.ts
@@ -11,7 +11,7 @@ export class CdkEc2SchedulerStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     const stopRule = new events.Rule(this, "StopEC2Instances", {
-      schedule: events.Schedule.expression("cron(0 16 ? * MON-FRI *)"),
+      schedule: events.Schedule.expression("cron(0 15 ? * MON-FRI *)"),
       description: "Stop EC2 instance at 10pm UTC+7 weekday.",
     });
     // stop the document database cluster.
@@ -20,7 +20,10 @@ export class CdkEc2SchedulerStack extends cdk.Stack {
         service: "EC2",
         action: "stopInstances",
         parameters: {
-          InstanceIds: []
+          InstanceIds: [
+            "i-0bdb7531eb547277f",
+            "i-019e947c18e99571a"
+          ]
         },
         policyStatement: new iam.PolicyStatement({
           effect: iam.Effect.ALLOW,
@@ -31,7 +34,7 @@ export class CdkEc2SchedulerStack extends cdk.Stack {
     );
     // start the cluster at 8am UTC+7 weekday.
     const startRule = new events.Rule(this, "StartsEC2Instances", {
-      schedule: events.Schedule.expression("cron(0 0 ? * MON-FRI *)"),
+      schedule: events.Schedule.expression("cron(0 1 ? * MON-FRI *)"),
       description: "Start EC2 instances at 8am UTC+7 weekday.",
     });
     // start the document database cluster.
@@ -40,7 +43,10 @@ export class CdkEc2SchedulerStack extends cdk.Stack {
         service: "EC2",
         action: "startInstances",
         parameters: {
-          InstanceIds: []
+          InstanceIds: [
+            "i-0bdb7531eb547277f",
+            "i-019e947c18e99571a"
+          ]
         },
         policyStatement: new iam.PolicyStatement({
           effect: iam.Effect.ALLOW,
