7e28d3bc034e9c7728a867137abc9af094bc181a
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2024-08-07 09:48:18 +0700

    feat: init

---
+++ lib/cdk-ec2-scheduler-stack.ts
@@ -0,0 +1,53 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+export class CdkEc2SchedulerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    const stopRule = new events.Rule(this, "StopEC2Instances", {
+      schedule: events.Schedule.expression("cron(0 16 ? * MON-FRI *)"),
+      description: "Stop EC2 instance at 10pm UTC+7 weekday.",
+    });
+    // stop the document database cluster.
+    stopRule.addTarget(
+      new targets.AwsApi({
+        service: "EC2",
+        action: "stopInstances",
+        parameters: {
+          InstanceIds: []
+        },
+        policyStatement: new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ["ec2:StopInstances"],
+          resources: ["*"],
+        }),
+      })
+    );
+    // start the cluster at 8am UTC+7 weekday.
+    const startRule = new events.Rule(this, "StartsEC2Instances", {
+      schedule: events.Schedule.expression("cron(0 0 ? * MON-FRI *)"),
+      description: "Start EC2 instances at 8am UTC+7 weekday.",
+    });
+    // start the document database cluster.
+    startRule.addTarget(
+      new targets.AwsApi({
+        service: "EC2",
+        action: "startInstances",
+        parameters: {
+          InstanceIds: []
+        },
+        policyStatement: new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ["ec2:StartInstances"],
+          resources: ["*"],
+        }),
+      })
+    );
+  }
+}
---
+++ test/cdk-ec2-scheduler.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEc2Scheduler from '../lib/cdk-ec2-scheduler-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ec2-scheduler-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEc2Scheduler.CdkEc2SchedulerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
